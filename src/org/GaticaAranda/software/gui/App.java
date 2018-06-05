34fbcd5dccd4c2797fc56e441b271e776fd95aa8
M-Aranda <cheloz_20@hotmail.com>
2018-06-05 03:14:12 -0300

    creada clase que permite alterar el color de las barras dependiendo del valor que tengan

--- src/org/GaticaAranda/software/gui/App.java
+++ src/org/GaticaAranda/software/gui/App.java
@@ -5,6 +5,14 @@
  */
 package org.GaticaAranda.software.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.LinearGradientPaint;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,6 +22,11 @@ import java.util.Calendar;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JComponent;
+import javax.swing.JProgressBar;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicProgressBarUI;
 
 /**
  *
@@ -29,12 +42,27 @@ public class App extends javax.swing.JFrame {
         iniciarRetomarInformacion();
         setLocationRelativeTo(null);
         setResizable(false);
+        personalizarBarrasDeProgreso();
 
         /*
         metodo para que inicializa hilos y asigna valores a los progress bar
         probarHilos();
          */
         probarHilos();
+    }
+
+    public void personalizarBarrasDeProgreso() {
+        barDiversion.setUI(new GradientPalletProgressBarUI(barDiversion));
+        barEnergia.setUI(new GradientPalletProgressBarUI(barEnergia));
+        barHambre.setUI(new GradientPalletProgressBarUI(barHambre));
+        barSalud.setUI(new GradientPalletProgressBarUI(barSalud));
+        Border bor = new LineBorder(Color.BLACK);
+
+        barDiversion.setBorder(bor);
+        barEnergia.setBorder(bor);
+        barHambre.setBorder(bor);
+        barSalud.setBorder(bor);
+
     }
 
     /**
@@ -644,6 +672,82 @@ public class App extends javax.swing.JFrame {
             }
         }
 
+    }
+
+    class GradientPalletProgressBarUI extends BasicProgressBarUI {
+
+        private final int[] pallet;
+        private JProgressBar barra;
+
+        public GradientPalletProgressBarUI(JProgressBar barra) {
+            super();
+            this.barra = barra;
+            this.pallet = makeGradientPallet();
+        }
+
+        private int[] makeGradientPallet() {
+            BufferedImage image = new BufferedImage(100, 1, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2 = image.createGraphics();
+            Point2D start = new Point2D.Float(0f, 0f);
+            Point2D end = new Point2D.Float(99f, 0f);
+            float[] dist = {0.0f, 0.5f, 1.0f};
+            Color[] colors = {Color.RED, Color.ORANGE, Color.GREEN};
+            g2.setPaint(new LinearGradientPaint(start, end, dist, colors));
+            g2.fillRect(0, 0, 100, 1);
+            g2.dispose();
+
+            int width = image.getWidth(null);
+            int[] pallet = new int[width];
+            PixelGrabber pg = new PixelGrabber(image, 0, 0, width, 1, pallet, 0, width);
+            try {
+                pg.grabPixels();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return pallet;
+        }
+
+        private Color getColorFromPallet(int[] pallet, float x) {
+            if (x < 0.0 || x > 1.0) {
+                throw new IllegalArgumentException("Parameter outside of expected range");
+            }
+            int i = (int) (pallet.length * x);
+            int max = pallet.length - 1;
+            int index = i < 0 ? 0 : i > max ? max : i;
+            int pix = pallet[index] & 0x00ffffff | (0x64 << 24);
+            return new Color(pix, true);
+        }
+
+        @Override
+        public void paintDeterminate(Graphics g, JComponent c) {
+            if (!(g instanceof Graphics2D)) {
+                return;
+            }
+            Insets b = barra.getInsets(); // area for border
+            int barRectWidth = barra.getWidth() - (b.right + b.left);
+            int barRectHeight = barra.getHeight() - (b.top + b.bottom);
+            if (barRectWidth <= 0 || barRectHeight <= 0) {
+                return;
+            }
+            int cellLength = getCellLength();
+            int cellSpacing = getCellSpacing();
+            // amount of progress to draw
+            int amountFull = getAmountFull(b, barRectWidth, barRectHeight);
+
+            if (barra.getOrientation() == JProgressBar.HORIZONTAL) {
+                // draw the cells
+                float x = amountFull / (float) barRectWidth;
+                g.setColor(getColorFromPallet(pallet, x));
+                g.fillRect(b.left, b.top, amountFull, barRectHeight);
+
+            } else { // VERTICAL
+                //...
+            }
+            // Deal with possible text painting
+            if (barra.isStringPainted()) {
+                paintString(g, b.left, b.top, barRectWidth, barRectHeight, amountFull, b);
+            }
+        }
     }
 
 }
